7ab711adaa09766c076222c3878a1b4b73b1a96b
policarpofelipe <policarpofelipe@gmail.com>
2025-09-30 08:57:22 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ import cargosRoutes from "./routes/cargos.js";
 import funcionariosRoutes from "./routes/funcionarios.js";
 import usuariosRoutes from "./routes/usuarios.js";
 import perfisRoutes from "./routes/perfis.js";
+import permissoesRoutes from "./routes/permissoes.js";
+
 
 const CONFIG = {
   JWT_SECRET:
@@ -116,6 +118,7 @@ app.use("/api/cargos", cargosRoutes);
 app.use("/api/funcionarios", funcionariosRoutes);
 app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/perfis", perfisRoutes);
+app.use("/api/permissoes", permissoesRoutes);
 
 // Healthcheck
 app.get("/health", (_req, res) => {
